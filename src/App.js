ed4de82b3f577b0fb69490951d20ef5c782e1092
parunkov <parunkov@gmail.com>
2023-11-24 15:02:14 +0500

    fonts added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className='firstScreen'>
+        <div className='container'>
+          <Header />
+          <h1 className='title'>участвуй
+            в акции и
+            выигрывай призы!</h1>
+            <button className='button'>участвовать</button>
+        </div>
+      </div>
+      <div className='secondScreen'>
+        <div className='container'>
+          <h2 className='secondTitle'>Выигрывай призы</h2>
+        </div>
+      </div>
     </div>
   );
 }
