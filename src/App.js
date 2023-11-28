8773f3f819823951f681b58cc045f30f082caa87
parunkov <parunkov@gmail.com>
2023-11-28 10:33:56 +0500

    prizes styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import styles from './App.module.scss';
 import Header from './components/header/Header';
-import { wifi } from './assets/wifi';
+import { wifi, spiral } from './assets/svg';
 
 function App() {
+  const pizes = [
+    {image: './assets/scooter.png', text: 'Электросамокат'},
+    {image: './assets/alisa.png', text: 'Колонка'},
+    {image: './assets/wach.png', text: 'Смарт часы'}
+  ];
+
   return (
     <div className={styles.app}>
       <div className={styles.firstScreen}>
@@ -17,13 +23,13 @@ function App() {
             <img src={require('./assets/leaf.png')} alt="Призы" className={styles.leaf} />
           </div>
           <Header />
-          <h1 className={styles.title} dangerouslySetInnerHTML={{__html: 'участвуй<br>в акции и выигрывай призы!'}} />
+          <h1 className={styles.title} dangerouslySetInnerHTML={{ __html: 'участвуй<br>в акции и выигрывай призы!' }} />
           <button className={styles.button}>участвовать</button>
           <div className={styles.card}>
             <div className={styles.cardImageWrapper}>
               <img src={require('./assets/card2.png')} alt="Чип" className={styles.cardImage} />
             </div>
-            <div className={styles.cardWifiWrapper} dangerouslySetInnerHTML={{__html: wifi}} />
+            <div className={styles.cardWifiWrapper} dangerouslySetInnerHTML={{ __html: wifi }} />
             <div className={styles.cardButton}>Главный приз</div>
             <div className={styles.cardSumm}>500 000 ₽</div>
           </div>
@@ -31,7 +37,26 @@ function App() {
       </div>
       <div className={styles.secondScreen}>
         <div className={styles.container}>
+          <div className={styles.spiralWrapper} dangerouslySetInnerHTML={{ __html: spiral }} />
           <h2 className={styles.secondTitle}>Выигрывай призы</h2>
+          <h4 className={styles.guaranteeTitle}>Гарантированный приз</h4>
+          <div className={styles.guarantee}>
+            <div className={styles.guaranteeContainer}>
+              <div className={styles.quaranteeImageWrapper}>
+                <img src={require('./assets/yandex.png')} alt="Призы" className={styles.quaranteeImage} />
+              </div>
+              <div className={styles.guaranteeText}>Зарегистрируйте один чек
+                и получите подписку
+                Яндекс Плюс на 1 год</div>
+            </div>
+          </div>
+          <h4 className={styles.prizesTitle}>Ежемесячный приз</h4>
+          <div className={styles.prizesContainer}>
+            {pizes.map((item) =><div className={styles.prizesItem}>
+              <img src={require(`${item.image}`)} alt={item.text} className={styles.prizesImage} />
+              <div className={styles.prizesText}>{item.text}</div>
+            </div>)}
+          </div>
         </div>
       </div>
     </div>
