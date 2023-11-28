02138fe096f7b4d4444f139c1403b21819d79122
parunkov <parunkov@gmail.com>
2023-11-28 11:25:39 +0500

    footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import { navData } from '../../data/navData';
 function Footer() {
     return (
         <div className={styles.footer}>
-
+            {navData.map((item) => <a href={item.link} className={styles.navLink} key={item.text}>{item.text}</a>)}
         </div>
     )
 };
