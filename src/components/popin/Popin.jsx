fb41a8015ceb6df5ea0e1c2ae44e5d3bb8a45c89
parunkov <parunkov@gmail.com>
2023-11-30 11:47:24 +0500

    popin markup

--- src/components/popin/Popin.jsx
+++ src/components/popin/Popin.jsx
@@ -5,6 +5,33 @@ function Popin() {
         <div className={styles.overlay}>
             <div className={styles.popin}>
                 <div className={styles.popinTitle}>регистрация</div>
+                <form className={styles.form}>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="name" className={styles.inputLabel}>ФИО</label>
+                        <input type="text" name="name" id="name" className={styles.input} required />
+                        <div className={styles.errorMessage}>Неверный формат ввода поля</div>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="login" className={styles.inputLabel}>E-mail</label>
+                        <input type="text" name="login" id="login" className={styles.input} required />
+                        <div className={styles.errorMessage}>Неверный формат ввода поля</div>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="phone" className={styles.inputLabel}>Телефон</label>
+                        <input type="text" name="phone" id="phone" className={styles.input} required />
+                        <div className={styles.errorMessage}>Неверный формат ввода поля</div>
+                    </div>
+                    <div className={styles.checkboxGroup}>
+                        <input type="checkbox" name="rules1" id="rules1" className={styles.checkbox} />
+                        <label htmlFor="rules1" className={styles.checkboxLabel}>Я согласен с <a href="/">правилами акции</a>, <a href="/">
+                            пользовательским
+                                соглашением
+                        </a>
+                            и с <a href="/">положением о конфиденциальности</a></label>
+                    </div>
+                    <button type="submit">Зарегистрироваться</button>
+                </form>
+                <div className={styles.popinText}>Есть аккаунт? <a href="/">Авторизуйся</a></div>
             </div>
         </div>
     );
